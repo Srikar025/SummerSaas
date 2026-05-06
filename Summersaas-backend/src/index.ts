f6acf7ec9582842e8a559dcc8d150a6bd75700fb
Srikar Gadagoju <you@example.com>
2026-05-06 09:03:02 +0530

    cors updated

--- Summersaas-backend/src/index.ts
+++ Summersaas-backend/src/index.ts
@@ -6,11 +6,13 @@ import { JWT_PASSWORD } from "./config.js";
 import { ContentModel } from "./db.js";
 import { userMiddleware } from "./middleware.js";
 
+
 const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ""
+    origin: "https://summer-saas-o1visosin-srikar025s-projects.vercel.app/",
+    credentials: true
 }));
 
 app.post("/api/v2/login", async (req, res) => {
